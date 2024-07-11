8dc468b0e1d4f13c806945218e52f62bddb56e8f
edwinntaadu <108189065+edwinntaadu@users.noreply.github.com>
2024-07-11 17:48:46 +0200

    Password Reset: Request a Reset

---
+++ components/RequestReset.js
@@ -0,0 +1,62 @@
+import useForm from "@/lib/useForm";
+import Form from "./styles/Form";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import { CURRENT_USER_QUERY } from "./User";
+import Error from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email)
+}
+`;
+
+export default function RequestReset() {
+    const {inputs, handleChange, resetForm} = useForm({
+        email: '',
+    });
+    const [signup, {data, loading, error}] = useMutation(REQUEST_RESET_MUTATION, {
+        variables: inputs,
+        // refetch the currently logged in user
+        // refetchQueries: [{query: CURRENT_USER_QUERY}]
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault(); // stop the form from submitting
+        console.log(inputs);
+        const res = await signup().catch(console.error);
+        console.log(res);
+        console.log({data, loading, error})
+        resetForm();
+        // Send the email and password to the graphqlAPI
+    }
+    //const error = data?.authenticateUserWithPassword.__typename ===
+    //"UserAuthenticationWithPasswordFailure"
+    // ? data?.authenticateUserWithPassword
+    // : undefined;
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+        <h2>Reset Password</h2>
+        <Error error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === true && (
+            <p>
+                Success! Check your email for a link!
+            </p>
+        )}
+        <label htmlFor="email">
+            Email
+            <input
+             type="email" 
+             name="email" 
+             placeholder="Your Email Address" 
+             autoComplete="email" 
+             value={inputs.email}
+             onChange={handleChange}
+            />
+        </label>
+        <button type="submit">Request Reset!</button>
+      </fieldset>
+    </Form>
+  )
+}
