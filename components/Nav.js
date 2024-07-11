cc67c7ec8357d17fec3510a5627a05da5f230d01
edwinntaadu <108189065+edwinntaadu@users.noreply.github.com>
2024-07-11 13:10:07 +0200

    Create a SignOut component

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import { useUser } from "./User";
+import SignOut from "./SignOut";
 
 
 export default function Nav() {
@@ -13,6 +14,7 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
       {!user && (
